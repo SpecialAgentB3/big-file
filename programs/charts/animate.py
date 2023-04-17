9b351fc57181f8aa2aa6efa3e2fb01bf114511db
Ben <70361638+SpecialAgentB3@users.noreply.github.com>
2023-04-17 03:22:24 -0500

    f

--- programs/charts/animate.py
+++ programs/charts/animate.py
@@ -20,26 +20,31 @@ column_names = [
 
 df = pd.read_csv(r'C:\Users\Ben\Desktop\Everything\programs\charts\responses.csv', header=None, names=column_names)
 
-df['item_1_price_ending'] = (df['item_1_fake_price'] % 1) * 100
-df['item_2_price_ending'] = (df['item_2_fake_price'] % 1) * 100
+df['item_1_price_ending'] = np.round((df['item_1_fake_price'] % 1) * 100).astype(int)
+df['item_2_price_ending'] = np.round((df['item_2_fake_price'] % 1) * 100).astype(int)
 
-df_long = pd.melt(df, id_vars=['id', 'selected_item'], value_vars=['item_1_price_ending', 'item_2_price_ending'], var_name='item', value_name='price_ending')
+def get_selection_rates(df, max_id):
+    df_filtered = df[df['id'] <= max_id].copy()
 
-def get_selection_rates(df):
-    df = df.copy()  # Create a copy of the input DataFrame to avoid the warning
-    df.loc[:, 'is_selected'] = df['selected_item'].apply(lambda x: f'item_{x}_price_ending') == df['item']
-    df['is_selected'] = df['is_selected'].astype(int)
-
-    grouped = df.groupby(np.round(df['price_ending'])).agg(
-        {'is_selected': 'sum', 'id': 'count'}
+    selected_counts = df_filtered[df_filtered['selected_item'].isin([1, 2])].apply(
+        lambda x: f'item_{x["selected_item"]}_price_ending', axis=1
     )
+    
+    selected_counts = pd.concat([selected_counts, df_filtered.loc[selected_counts.index, 'selected_item']], axis=1)
+    selected_counts.columns = ['item', 'selected_item']
+    
+    selected_counts = selected_counts.groupby(['item', 'selected_item']).size().reset_index(name='counts')
+    selected_counts.set_index('item', inplace=True)
 
-    selection_rates = (grouped['is_selected'] / grouped['id']).fillna(0)
+    total_presented_1 = df_filtered.groupby(np.round(df_filtered['item_1_price_ending'])).size()
+    total_presented_2 = df_filtered.groupby(np.round(df_filtered['item_2_price_ending'])).size()
+    total_presented = total_presented_1.add(total_presented_2, fill_value=0)
 
+    selection_rates = selected_counts['counts'].divide(total_presented, fill_value=0)
     return selection_rates
 
 
-df_list = [df_long[df_long['id'] <= i] for i in range(1, len(df) + 1)]
+
 
 fig, ax = plt.subplots()
 price_endings = list(range(0, 100))
@@ -55,10 +60,10 @@ def update_bar_heights(selection_rates):
         bar.set_height(selection_rates.get(idx, 0))
 
 def animate(i):
-    df_filtered = df_list[i]
-    selection_rates = get_selection_rates(df_filtered)
+    max_id = i + 1
+    selection_rates = get_selection_rates(df, max_id)
     update_bar_heights(selection_rates)
 
-ani = animation.FuncAnimation(fig, animate, frames=len(df_list), interval=50, repeat=False)
+ani = animation.FuncAnimation(fig, animate, frames=len(df), interval=100, repeat=False)
 
 plt.show()
